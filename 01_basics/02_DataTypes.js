fbbb85d5da4f7bddb8975376246b348105945ac0
MonishaChakraborty <monishachakrabarty84@gmail.com>
2024-11-25 15:44:10 +0000

    done

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all codes as newer version
+
+//alert(3+3) // we are using nodejs not browser
+
+let name="Monisha"
+let age= 20
+let isLoggedin= false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value/ empty value
+//undefined => value not assigned yet
+//symbol => to find uniqueness
+
+
+//object
+
+console.log( typeof null); //output: object
+console.log(typeof undefined)// undefined
