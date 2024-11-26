9566a9423873c44b4d7e08d80c9f1e5543968369
MonishaChakraborty <monishachakrabarty84@gmail.com>
2024-11-26 19:14:20 +0000

    DataType summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,21 @@
+//Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id==anotherId);
+
+
+
+//Reference / non primitive
+
+// array, objects , functions
+
+
+//Creating object
+let myObj=
+{
+    name: "monisha",
+    age: 20,
+}
