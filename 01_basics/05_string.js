2de0fbea15f2f0ade2f6e753344ce053b730cfe4
MonishaChakraborty <monishachakrabarty84@gmail.com>
2024-11-29 14:45:38 +0000

    Done

---
+++ 01_basics/05_string.js
@@ -0,0 +1,8 @@
+const name= "Monisha"
+const repoCount= 50
+//console.log(name + repoCount);
+console.log(`Hello my name is ${name}  and my repoCount is ${repoCount}`);
+const gameName=new String("monisha")
+console.log(gameName[0])
+console.log(gameName.__proto__);
+
